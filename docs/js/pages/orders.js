cbd80d6b111101416e77be9855b1c881ac01bd15
Liam <liamnagle5@gmail.com>
2026-02-19 13:06:23 +0000

    Added tracking number

--- docs/js/pages/orders.js
+++ docs/js/pages/orders.js
@@ -228,7 +228,8 @@ async function handleFormSubmit(e) {
         total_price: parseFloat(document.getElementById('totalPrice').value),
         status: document.getElementById('status').value,
         delivery_type: document.getElementById('deliveryType').value,
-        notes: document.getElementById('notes').value.trim() || null
+        notes: document.getElementById('notes').value.trim() || null,
+        tracking_number: document.getElementByID('tracking_number').value
     };
 
     try {
